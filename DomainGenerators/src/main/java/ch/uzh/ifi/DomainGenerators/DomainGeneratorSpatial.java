f18e70c6b72cbf0b14af5b9a8c98c9b2aa625c3e
dmitrymoor <dmitry.moor@yandex.ru>
2016-04-01 22:17:17 +0200

    renamed

---
+++ DomainGenerators/src/main/java/ch/uzh/ifi/DomainGenerators/DomainGeneratorSpatial.java
@@ -0,0 +1,74 @@
+package ch.uzh.ifi.DomainGenerators;
+
+import java.util.List;
+
+import ch.uzh.ifi.MechanismDesignPrimitives.AtomicBid;
+import ch.uzh.ifi.MechanismDesignPrimitives.CombinatorialType;
+import ch.uzh.ifi.MechanismDesignPrimitives.IDomainGenerator;
+import ch.uzh.ifi.MechanismDesignPrimitives.Type;
+import ch.uzh.ifi.DomainGenerators.SpatialDomainGenerator;
+import ch.uzh.ifi.GraphAlgorithms.Graph;
+
+/**
+ * The class generates bids using same approach as in CATS "regions".
+ * @author Dmitry Moor
+ */
+public class DomainGeneratorSpatial implements IDomainGenerator
+{
+	/**
+	 * A simple constructor.
+	 * @param numberOfGoods the number of goods in the auction
+	 * @throws SpacialDomainGenerationException if cannot create a square grid with the specified number of goods
+	 */
+	public DomainGeneratorSpatial(int numberOfGoods) throws SpacialDomainGenerationException
+	{
+		_numberOfGoods = numberOfGoods;
+		generateGrid();
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * @see ch.uzh.ifi.MechanismDesignPrimitives.IDomainGenerator#generateBid(long, int)
+	 */
+	@Override
+	public Type generateBid(long seed, int agentId)
+	{
+		_spatialDomainGenerator = new SpatialDomainGenerator(_numberOfGoods, _grid, agentId);
+		try
+		{
+			_spatialDomainGenerator.generateSpacialBids(seed);
+		}
+		catch (SpacialDomainGenerationException e)
+		{
+			e.printStackTrace();
+		}
+		
+		List<AtomicBid> bid = _spatialDomainGenerator.getBid();
+		if(bid.size() == 0)  throw new RuntimeException("No atoms produced");
+
+		Type ct = new CombinatorialType();
+		bid.stream().forEach( i -> ct.addAtomicBid(i) );
+		return ct;
+	}
+	
+	/**
+	 * The method generates a rectangular spatial proximity graph (grid)
+	 * @throws SpacialDomainGenerationException if cannot create a square grid
+	 */
+	private void generateGrid() throws SpacialDomainGenerationException
+	{
+		int nRows = (int)Math.round( Math.sqrt(_numberOfGoods));
+		int nCols = _numberOfGoods / nRows;
+		
+		if( nRows * nCols != _numberOfGoods ) throw new SpacialDomainGenerationException("Error when computing grid dimensions: nRows=" + nRows + " nCols="+nCols);
+		
+		GridGenerator gridGenerator = new GridGenerator(nRows, nCols);
+		gridGenerator.setSeed(0);
+		gridGenerator.buildProximityGraph();
+		_grid = gridGenerator.getGrid();
+	}
+	
+	protected int _numberOfGoods;										//Number of goods in the auction
+	protected Graph _grid;												//Spatial proximity graph
+	protected SpatialDomainGenerator _spatialDomainGenerator;			//Spatial domain generator
+}
