56b03cf6af6d226468ad5e946721939c747e59b8
dmitrymoor <dmitry.moor@yandex.ru>
2016-04-13 15:16:19 +0200

    + 1 unittest

--- DomainGenerators/src/test/java/ch/uzh/ifi/DomainGenerators/testJointProbabilityMass.java
+++ DomainGenerators/src/test/java/ch/uzh/ifi/DomainGenerators/testJointProbabilityMass.java
@@ -2,6 +2,7 @@ package ch.uzh.ifi.DomainGenerators;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -228,5 +229,91 @@ public class testJointProbabilityMass
 		realizedRVs.add(0.8);
 		prob = jpmf.getMarginalProbability(bundle, allocatedGoods, realizedRVs);
 		assertTrue( Math.abs( prob - 0.7) < 1e-2 );
+	}
+	
+	/**
+	 * The test is used to find the number of bombs to throw in order to get a desired expected
+	 * availability of a node in a setting with 16 nodes.
+	 */
+	@Test
+	public void testGridConditional16() 
+	{
+		int numberOfRows = 4;
+		int numberOfColumns = 4;
+		int numberOfBombsToThrow = 5;
+		GridGenerator generator = new GridGenerator(numberOfRows, numberOfColumns);
+		generator.setSeed(0);
+		generator.buildProximityGraph();
+		Graph grid = generator.getGrid(); 
+		
+		//System.out.println("Grid: \n" + grid.toString());
+		
+		JointProbabilityMass jpmf = new JointProbabilityMass( grid );
+		jpmf.setNumberOfSamples(1000);
+		jpmf.setNumberOfBombsToThrow(1);
+		
+		IBombingStrategy b1 = new FocusedBombingStrategy(grid, 1., 0.3, 0.2);
+		//IBombingStrategy b2 = new FocusedBombingStrategy(grid, 1., 0.4, 0.3);
+		List<IBombingStrategy> bombs = new ArrayList<IBombingStrategy>();
+		bombs.add(b1);
+		//bombs.add(b2);
+		
+		List<Double> probDistribution = new ArrayList<Double>();
+		probDistribution.add(1.0);
+		//probDistribution.add(0.5);
+		
+		jpmf.setBombs(bombs, probDistribution);
+		jpmf.setNumberOfBombsToThrow(numberOfBombsToThrow);
+		jpmf.update();
+		
+		List<Integer> bundle = new ArrayList<Integer>();
+		bundle.add(1);
+		double expCorner = jpmf.getMarginalProbability(bundle, null, null);
+		
+		bundle.clear();
+		bundle.add(2);
+		double expEdge = jpmf.getMarginalProbability(bundle, null, null);
+		
+		bundle.clear();
+		bundle.add(5);
+		double expInner = jpmf.getMarginalProbability(bundle, null, null);
+		
+		double expectedAvailability = 0.25 * expCorner + 0.5 * expEdge + 0.25 * expInner; 
+		System.out.println("E[a]="+expectedAvailability);
+		//assertTrue(Math.abs( prob - 0.7) < 1e-2);
+		
+		//List<Integer> allocatedGoods = new LinkedList<Integer>();
+		//List<Double> realizedRVs = new LinkedList<Double>();
+		//allocatedGoods.add(2);
+		//realizedRVs.add(0.7);
+		
+		//prob = jpmf.getMarginalProbability(bundle, allocatedGoods, realizedRVs);
+		//System.out.println("prob="+prob);
+		/*assertTrue(Math.abs( prob - 0.7) < 1e-2);
+		
+		realizedRVs.clear();
+		realizedRVs.add(0.6);
+		assertTrue(Math.abs( prob - 0.7) < 1e-2);
+		
+		realizedRVs.clear();
+		realizedRVs.add(0.8);
+		prob = jpmf.getMarginalProbability(bundle, allocatedGoods, realizedRVs);
+		assertTrue(Math.abs( prob - 0.7) < 1e-2);
+		
+		bundle.add(2);
+		prob = jpmf.getMarginalProbability(bundle, null, null);
+		assertTrue(Math.abs( prob - 0.65) < 1e-2);
+		
+		allocatedGoods.clear();
+		allocatedGoods.add(1);
+		realizedRVs.clear();
+		realizedRVs.add(0.7);
+		prob = jpmf.getMarginalProbability(bundle, allocatedGoods, realizedRVs);
+		assertTrue( Math.abs( prob - 0.65) < 1e-2 );
+		
+		allocatedGoods.add(2);
+		realizedRVs.add(0.8);
+		prob = jpmf.getMarginalProbability(bundle, allocatedGoods, realizedRVs);
+		assertTrue( Math.abs( prob - 0.7) < 1e-2 );*/
 	}
 }
