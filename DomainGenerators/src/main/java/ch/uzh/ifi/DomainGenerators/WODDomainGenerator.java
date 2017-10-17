dc88797de2ef4b3d010141a4c03cc71662f161da
dmitrymoor <dmitry.moor@yandex.ru>
2017-10-17 12:14:29 +0200

    some updates

--- DomainGenerators/src/main/java/ch/uzh/ifi/DomainGenerators/WODDomainGenerator.java
+++ DomainGenerators/src/main/java/ch/uzh/ifi/DomainGenerators/WODDomainGenerator.java
@@ -1,6 +1,13 @@
 package ch.uzh.ifi.DomainGenerators;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.apache.commons.math3.distribution.*;
+
+import ch.uzh.ifi.MechanismDesignPrimitives.AtomicBid;
+import ch.uzh.ifi.MechanismDesignPrimitives.Distribution;
+import ch.uzh.ifi.MechanismDesignPrimitives.ParametrizedQuasiLinearAgent;
 /**
  * This class generates domains for the data market (see AAMAS'17)
  * @author Dmitry
@@ -25,12 +32,30 @@ public class WODDomainGenerator
 		
 		_costL = costL;
 		_costH = costH;
+		_costDistribution = Distribution.UNIFORM;
+		
+		_sellers = new LinkedList<AtomicBid>();
+	}
+	
+	/**
+	 * The method generates the WoD domain.
+	 */
+	public void generate()
+	{
+		for(int i = 0; i < _numberOfSellers; ++i)
+		{
+			
+		}
 	}
 	
 	private int _numberOfDBs;					//Number of data bases
 	private int _numberOfSellers;				//Number of sellers
 	private int _numberOfBuyers;				//Number of buyers
 	
-	private double _costL;
-	private double _costH;
+	private double _costL;						//Lower bound on costs
+	private double _costH;						//Upper bound on costs
+	private Distribution _costDistribution;
+	
+	List<ParametrizedQuasiLinearAgent> _buyers;	//List of buyers
+	List<AtomicBid> _sellers;					//List of sellers' types
 }
